91ab5dd574653bfdb436c85c89443b7973f0d2cf
ejimenez22 <ejimenez2292@gmail.com>
2022-02-19 09:23:34 -0800

    added useroptions

--- assets/password.js
+++ assets/password.js
@@ -20,13 +20,37 @@ var passwordNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 
 function generatePassword() {
+
+};
+
+function getOptions() {
   var passwordLength = window.prompt("Please enter how many characters you would like to have, between 8 and 128.");
   while (passwordLength >= 8 || passwordLength <= 128) {
   }
   var lowerCase = window.confirm("Would you like to add lower case letters?");
+  if (lowerCase === false) {
+    return null;
+
+  }
   var upperCase = window.confirm("Would you like to add upper case letters?");
+  if (upperCase === false) {
+    return null;
+
+  }
   var passwordNumbers = window.confirm("Would you like numbers?");
+  if (passwordNumbers === false) {
+    return null;
+
+  }
   var specialCharacters = window.confirm("Would you like to add special characters?");
-};
+  if (specialCharacters === false) {
+    return null;
 
+  }
+}
 
+// getOptions values need to go here 
+var userOptions = []
+function getRandom() {
+Math.floor(Math.random() * userOptions.array)
+}
