fc9609e213ffb486518598f9dc6ca8dc43a98eca
ejimenez22 <ejimenez2292@gmail.com>
2022-02-20 00:49:28 -0600

    struggling

--- assets/password.js
+++ assets/password.js
@@ -19,31 +19,51 @@ var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 var passwordNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 
+// create password
 function generatePassword() {
+
+  var randomPassword = "";
+  var passwordCharacters = "";
+
   var passwordLength = window.prompt("Please enter how many characters you would like to have, between 8 and 128.");
-  while (passwordLength >= 8 || passwordLength <= 128)
-  var lowerCase = window.confirm("Would you like to add lower case letters?")
-  var upperCase = window.confirm("Would you like to add upper case letters?")
-  var passwordNumbers = window.confirm("Would you like numbers?")
-  var specialCharacters = window.confirm("Would you like to add special characters?")
+  if (passwordLength >= 8 || passwordLength <= 128) {
+    if (passwordLength < 8 || passwordLength >128) {
+      window.alert("Password must be between 8-128 characters!");
+      window.prompt("Please enter how many characters you would like to have, between 8 and 128.");
+    }
+  }
+  var lowerCase = window.confirm("Would you like to add lower case letters?");
+  if (lowerCase) {
+    randomPassword += lowercase;
+  }
+  var upperCase = window.confirm("Would you like to add upper case letters?");
+  if (upperCase) {
+    randomPassword += upperCase;
+  }
+  var passwordNumbers = window.confirm("Would you like numbers?");
+  if (passwordNumbers) {
+    randomPassword += passwordNumbers;
+  }
+  var specialCharacters = window.confirm("Would you like to add special characters?");
+  if (specialCharacters) {
+    randomPassword += specialCharacters;
+  }
 
-var getOptions = []
-
-if (lowerCase === true) {
+if (lowerCase) {
   randomPassword = randomPassword.concat(lowerCase);
 }
-if (upperCase === true) {
+if (upperCase) {
   randomPassword = randomPassword.concat(upperCase);
 }
-if (passwordNumbers === true) {
+if (passwordNumbers) {
   randomPassword = randomPassword.concat(passwordNumbers);
 }
-if (specialCharacters === true) {
+if (specialCharacters) {
   randomPassword = randomPassword.concat(specialCharacters);
 }
-var randomPassword = []
 
-for (var i = 0; i < randomPassword; i++) {
-  randomPassword = randomPassword + getOptions[Math.floor(Math.random() * getOptions.length)];
+for (var i = 0; i <= passwordLength; i++) {
+  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
 }
+return randomPassword;
 };
